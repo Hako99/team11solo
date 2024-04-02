9bd899d1d78132ba08295dea4352b33fe5bdb818
mine <mineki94@naver.com>
2024-04-02 09:57:13 +0900

    기본 프로젝트 생성

---
+++ src/main/java/com/example/team11solo/Team11soloApplication.java
@@ -0,0 +1,13 @@
+package com.example.team11solo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team11soloApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Team11soloApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/team11solo/Team11soloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team11solo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team11soloApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
